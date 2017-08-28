15989e79b62039928ba7bf3148f2e83ae41f023e
Aaron Wilhelm <aaron.j.wilhelm@gmail.com>
2017-08-27 21:19:16 -0700

    Revised getTranslation() to do all the different parts of speech as well as the ability to do the gender and report if a noun is plural

--- yandexTranslateHandler.py
+++ yandexTranslateHandler.py
@@ -6,21 +6,25 @@ key = "dict.1.1.20170823T200338Z.d66eba1c8ea121ff.d8d6268f2c83d0d80e2dd4a98cac17
 
 
 # slang and elang need to be in the two character language code that Yandex specifies
-# returns a 2D list where first list are the different definitions and the second lists are synonyms for that definition
+# returns an object where it is [every part of speech(word, [defs[synonyms for that def]], part of speech, gen, num)]
+# note that gen and num may not be supplied (i.e. if not a noun)
 def getTranslation(text, slang, elang):
     langCode = slang + "-" + elang
     parameters = {"key": key, "lang": langCode, "text": text}
     response = requests.get(baseUrl, params=parameters)
     data = response.json()
     definitions = []
-    for x in data["def"][0]["tr"]:
-        synonyms = []
-        synonyms.append(x["text"])
-        if "syn" in x:
-            for syn in x["syn"]:
-                synonyms.append(syn["text"])
-        definitions.append(synonyms)
+    for type in range(len(data["def"])):
+        typeDefs = []
+        for x in data["def"][type]["tr"]:
+            synonyms = []
+            synonyms.append(x["text"])
+            if "syn" in x:
+                for syn in x["syn"]:
+                    synonyms.append(syn["text"])
+            typeDefs.append(synonyms)
+        definitions.append((data["def"][type]["text"], typeDefs, data["def"][type]["pos"], data["def"][type].get("gen"), data["def"][type].get("num")))
     return definitions
 
 
-print(getTranslation("laufen", "de", "en"))
+# print(getTranslation("steuern", "de", "en"))
