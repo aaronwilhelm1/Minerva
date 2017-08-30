3d03334961b87738084e101c3caf2d0411f3c083
Aaron Wilhelm <aaron.j.wilhelm@gmail.com>
2017-08-29 20:04:50 -0700

    Added a language property for articles

--- article.py
+++ article.py
@@ -1,13 +1,18 @@
 
 
-#A data container for the articles
+# A data container for the articles
+# The language should be the 2 letter language code as outlined in globals
 class Article():
-    def __init__(self, title, text):
+    def __init__(self, title, text, language):
         self.title = title
         self.text = text
+        self.language = language
 
     def getTitle(self):
         return self.title
 
     def getText(self):
         return self.text
+
+    def getLanguage(self):
+        return self.language
